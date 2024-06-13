7bcada3933228fdbc0b661947d94f5e0653819fa
JesusOfLagos <jesuswrites20043@gmail.com>
2024-06-13 23:54:30 +0100

    made reset global

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Home } from './site/home';
 import { NotFound } from './notfound';
 import { SignIn } from './auth/main/signin';
 import { SignUp } from './auth/main/signup';
+import { ResetPassword } from './auth/main/resetPassword';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="*" element={<NotFound />} />
         <Route path='/login' element={<SignIn />} />
         <Route path='/register' element={<SignUp />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
   );
