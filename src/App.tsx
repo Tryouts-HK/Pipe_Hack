a0d5f16d77a771cbfa26e9270c70db7734b97975
JesusOfLagos <jesuswrites20043@gmail.com>
2024-06-13 23:57:32 +0100

    made change passowrd globall

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { SignIn } from './auth/main/signin';
 import { SignUp } from './auth/main/signup';
 import { ResetPassword } from './auth/main/resetPassword';
 import { ForgotPassword } from './auth/main/forgotPassword';
+import { ChangePassword } from './auth/main/changePassword';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path='/register' element={<SignUp />} />
         <Route path='/reset-password' element={<ResetPassword />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/change-password' element={<ChangePassword />} />
       </Routes>
     </BrowserRouter>
   );
