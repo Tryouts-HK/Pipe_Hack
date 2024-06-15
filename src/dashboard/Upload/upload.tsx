776343d6aba6ee8b583777e46d78cfa07b7ff5ce
JesusOfLagos <jesuswrites20043@gmail.com>
2024-06-15 23:18:26 +0100

    added upload

--- src/dashboard/Upload/upload.tsx
+++ src/dashboard/Upload/upload.tsx
@@ -2,6 +2,10 @@ export const Upload = () => {
     return (
         <div>
             <h1>Upload</h1>
+            <div>
+                <input type="file" />
+                <button>Upload</button>
+            </div>
         </div>
     )
 }
