80d58e18942586cf16d116a8d87a87da8d72d87d
JesusOfLagos <jesuswrites20043@gmail.com>
2024-06-15 22:57:16 +0100

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home } from './site/home';
 import { NotFound } from './notfound';
 import { SignIn } from './auth/main/signin';
 import { SignUp } from './auth/main/signup';
 import { ResetPassword } from './auth/main/resetPassword';
 import { ForgotPassword } from './auth/main/forgotPassword';
 import { ChangePassword } from './auth/main/changePassword';
+import { Site } from './site/site';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Site />} />
         <Route path="*" element={<NotFound />} />
         <Route path='/login' element={<SignIn />} />
         <Route path='/register' element={<SignUp />} />
